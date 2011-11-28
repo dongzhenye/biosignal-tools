6736a94bb7a32878ddc486d39a93f3b37028aeac
Alois Schlögl <alois.schloegl@gmail.com>
2011-11-28 13:21:15 +0000

    add test for ttl2trig

---
+++ biosig4c++/test0/test_ttl2trig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This file is for testing ttl2trig
+
+# Copyright 2011 Alois Schloegl, IST Austria <alois.schloegl@ist.ac.at>
+
+.,/bin/ttl2trig -c "date" -i hw:2,0 --chan=0 --Threshold=.25 
