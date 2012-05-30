a1331e9cbd0bbfd041e106166646ba820c9d4e83
Alois Schlögl <alois.schloegl@gmail.com>
2012-05-30 15:19:47 +0000

    call to save2gdf.exe instead of save2gdf

--- biosig4c++/win32/bin2rec.bat
+++ biosig4c++/win32/bin2rec.bat
@@ -11,4 +11,4 @@ echo BIN2REC converts BIN data into other biosignal data formats (default GDF)
 echo usage: bin2rec source dest
 echo usage: bin2rec -f=FMT source dest
 echo		FMT can be BDF,BIN,CFWB,EDF,GDF,HL7aECG,SCP_ECG
-save2gdf -f=GDF %1 %2 %3 %4 %5 %6 %7
+save2gdf.exe -f=GDF %1 %2 %3 %4 %5 %6 %7
--- biosig4c++/win32/rec2bin.bat
+++ biosig4c++/win32/rec2bin.bat
@@ -11,6 +11,6 @@ echo REC2BIN converts different biosignal data formats into the BIN format
 echo usage: rec2bin source dest
 echo usage: rec2bin -f=FMT source dest
 echo		FMT can be BDF,BIN,CFWB,EDF,GDF,HL7aECG,SCP_ECG
-save2gdf -f=BIN %1 %2 %3 %4 %5 %6 %7
+save2gdf.exe -f=BIN %1 %2 %3 %4 %5 %6 %7
 
 
