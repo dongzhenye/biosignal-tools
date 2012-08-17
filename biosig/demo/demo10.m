56f204cad2febbb55f68b3e14896386b0328f6c3
Alois Schlögl <alois.schloegl@gmail.com>
2012-08-17 21:02:29 +0000

    demo for deconvolution method for detection of spontaneous synaptic currents

---
+++ biosig/demo/demo10.m
@@ -0,0 +1,61 @@
+%% Implement deconvolution method as used in [1]
+%%
+%% References: 
+%%  [1] Alejandro Javier Pernía-Andrade, Sarit Pati Goswami, Yvonne Stickler, 
+%%      Ulrich Fröbe, Alois Schlögl, and Peter Jonas (submitted) 
+%%  A deconvolution-based method with high sensitivity and temporal
+%%  resolution for detection of spontaneous synaptic currents in vitro and
+%%  in vivo.
+
+
+%  $Id$
+%  Copyright (C) 2012 by Alois Schloegl  <alois.schloegl@ist.ac.at>  
+%  This is part of the BIOSIG-toolbox http://biosig.sf.net/
+
+% BioSig is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public License
+% as published by the Free Software Foundation; either version 3
+% of the  License, or (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of  
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+% USA.
+
+
+f1 = 'http://pub.ist.ac.at/~schloegl/download/rawdata.gdf';
+f2 = 'http://pub.ist.ac.at/~schloegl/download/template.gdf';
+
+while ~exist('rawdata.gdf','file') || ~exist('template.gdf','file')
+    fprintf(1,'Download example and template file from here:\n');
+    fprintf(1,'\t%s\n\t%s\n',f1,f2);
+    fprintf(1,'and save them in local working directory "%s".\n',pwd);
+    pause;
+end; 
+
+
+clear; 
+B = [0.1,150]; %% bandwidth 
+
+% load raw data
+[r,rHDR]=sload('rawdata.gdf');
+
+% load template
+[t,tHDR]=sload('template.gdf');
+
+fs = rHDR.SampleRate;
+
+% deconvolution and filteriong
+d = signal_deconvolution(r, t, fs, B); 
+	
+% display brief segment 
+t = [1:100000]'+0e5;
+plot(t/fs,[zscore(r), zscore(d)*5-10](t,:))
+legend({'Raw data','Deconvolved data'});
+
+
