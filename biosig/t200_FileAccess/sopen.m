ef83c61bad44170b590073616e712692cc3eb251
Alois Schlögl <alois.schloegl@gmail.com>
2012-08-25 13:33:27 +0000

    Persyst: implement in Matlab script

--- biosig/t200_FileAccess/sopen.m
+++ biosig/t200_FileAccess/sopen.m
@@ -4463,17 +4463,21 @@ elseif strcmp(HDR.TYPE,'OGG'),
         
 elseif strcmp(HDR.TYPE,'Persyst'),
         if any(HDR.FILE.PERMISSION=='r');
-                [HDR.FILE.FID] = fopen(HDR.FileName,[HDR.FILE.PERMISSION,'t']);          
+                [HDR.FILE.FID] = fopen(HDR.FileName,['rb']);          
                 if HDR.FILE.FID < 0, 
                         HDR.ErrNum = [32,HDR.ErrNum];
                         return;
                 end;
-                [H1,count] = fread(HDR.FILE.FID,[1,inf],'uint8=>char');
+                [H1,count] = fread(HDR.FILE.FID, [1,inf], 'uint8=>char');
 		fclose(HDR.FILE.FID); 
 
+		HDR.Endianity ='ieee-le';
+		HDR.FLAG.OVERFLOWDETECTION = 0; 		
+		flag_interleaved = 1;
 		status = 0; 
-		[line,r] = strtok(H1,'\n\r');
+		line = H1;
 		while (~isempty(line))
+			[line,H1] = strtok(H1,char([10,13]));
 			if strcmp(line,'[FileInfo]')
 				status = 1; 
 			elseif strcmp(line,'[ChannelMap]')
@@ -4498,30 +4502,37 @@ elseif strcmp(HDR.TYPE,'Persyst'),
 					val = val(2:end);
 					switch (tag)
 					case {'File'}
-						[p,datfile,ext] = fileparts(val); 
-						datfile = [datfile,ext];
+						val(val=='\')='/';
+						ix = find(val=='/');
+						if isempty(ix), ix = 0; end; 
+						datfile = val(ix(end)+1:end);
 					case {'FileType'}
-						if strcmp(val,'Interleaved')
-							HDR.SPR = 1; 
-						end;
+						flag_interleaved = strcmp(val,'Interleaved');
 					case {'SamplingRate'}
-						HDR.SampleRate = feval(val); 
+						HDR.SampleRate = str2double(val); 
 					case {'Calibration'}
-						HDR.Cal = feval(val); 
+						HDR.Cal = str2double(val); 
 					case {'WaveformCount'}
-						HDR.NS = feval(val); 
-					case {'DateType'}
-						switch feval(val)	
-						case {0}: gdftyp = 3;  
-							HDR.Endianity ='ieee-le';
-						case {4}: gdftyp = 3;  
+						HDR.NS = str2double(val); 
+					case {'DataType'}
+						switch (val)	
+						case {'0'} 
+							HDR.GDFTYP = 3;  
+							HDR.AS.bpb = 2*HDR.NS;
+						case {'4'}
+							HDR.GDFTYP = 3;  
+							HDR.AS.bpb = 2*HDR.NS;
 							HDR.Endianity = 'ieee-be';
-						case {6}: gdftyp = 1;  
+						case {'6'} 
+							HDR.GDFTYP = 1;  
+							HDR.AS.bpb = HDR.NS;
+						otherwise
+		
 						end;
 					end; 
 				case {2}
 					[tag,val]=strtok(line,'=');
-					ch = feval(val(2:end));
+					ch = str2double(val(2:end));
 					HDR.Label{ch} = tag; 
 				case {3}
 				case {4}
@@ -4531,8 +4542,8 @@ elseif strcmp(HDR.TYPE,'Persyst'),
 					[ign,ll]=strtok(ll,',');
 					[ign,ll]=strtok(ll,',');
 					HDR.EVENT.Desc{HDR.EVENT.N} = ll(2:end); 
-					HDR.EVENT.POS(HDR.EVENT.N) = feval(pos);	
-					HDR.EVENT.DUR(HDR.EVENT.N) = feval(pos);	
+					HDR.EVENT.POS(HDR.EVENT.N) = str2double(pos);	
+					HDR.EVENT.DUR(HDR.EVENT.N) = str2double(pos);	
 				case {5}
 					[tag,val]=strtok(line,'=');
 					val = val(2:end);
@@ -4544,38 +4555,67 @@ elseif strcmp(HDR.TYPE,'Persyst'),
 					case {'Last'}
 						SurName = val; 
 					case {'Hand'}
-						HDR.Patient.Handedness = any(val(1)=='rR') + any(val(2)=='lL') * 2;
+						HDR.Patient.Handedness = any(val(1)=='rR') + any(val(1)=='lL') * 2;
 					case {'Sex'}
-						HDR.Patient.Sex = any(val(1)=='mM') + any(val(2)=='fF') * 2;
+						HDR.Patient.Sex = any(val(1)=='mM') + any(val(1)=='fF') * 2;
 					case {'BirthDate'}
-						val(val=='/')=0;
+						val(val==47)=' ';
 						t = str2double(val);
-						HDR.T0 = [t([3,1,2]),0,0,0]; 
+						if t(3) < 30, 	c = 2000; 
+						else 		c = 1900; 
+						end; 
+						HDR.Patient.Birthday = [t(3)+c,t(1),t(2), 0, 0, 0]; 
 					case {'TestDate'}
-						val(val=='/')=0;
+						val(val=='/')=' ';
 						t = str2double(val);
-						HDR.T0([3,1,2]) = [t([3,1,2]),0,0,0]; 
+						if t(3) < 80, 	c = 2000; 
+						else 		c = 1900; 
+						end; 
+						HDR.T0(1:3) = [t(3)+c,t(1),t(2)]; 
 					case {'TestTime'}
-						val(val==':')=0;
+						val(val==':')=' ';
 						t = str2double(val);
 						HDR.T0(4:6) = t; 
 					case {'ID'}
 						HDR.Patient.Id = val;
-					case {'Physician'}
-						HDR.Physician = val;
+					%case {'Physician'}
+					%	This is not really needed
+					%	HDR.REC.Doctor = val;
 					case {'Technician'}
-						HDR.Technician = val;
+						HDR.REC.Technician = val;
 					case {'Medications'}
 						HDR.Patient.Medication = val;
 					end;
 				case {6}		
 				end;
 			end;
-			[line,r] = strtok(r,'\n\r');
 		end;
 
+		
+		HDR.FILE.POS = 0;
 		HDR.FILE.Ext = '.dat'; 
-		HDR.FILE.FID = fopen(fullfile(HDR.FILE.Path,datfile),'r', HDR.Endianity);
+		fid = fopen(fullfile(HDR.FILE.Path,datfile),'r', HDR.Endianity);
+		if HDR.GDFTYP==3, 
+			s = fread(fid,inf,'int16');
+		elseif HDR.GDFTYP==1
+			s = fread(fid,inf,'int8');
+		end 
+		fclose(fid); 
+		
+		if flag_interleaved,
+			HDR.data = reshape(s,HDR.NS,[])';
+		else 
+			HDR.data = reshape(s,[],HDR.NS);
+		end; 
+		HDR.data = HDR.data*HDR.Cal;
+
+		HDR.SPR     = 1; 
+		HDR.NRec    = size(HDR.data,1); 
+		HDR.TYPE    = 'native';	
+		HDR.DigMax  = max(HDR.data); 
+		HDR.DigMin  = min(HDR.data); 
+		HDR.PhysMax = HDR.Cal*HDR.DigMax;
+		HDR.PhysMin = HDR.Cal*HDR.DigMin;
 	end 
         
         
