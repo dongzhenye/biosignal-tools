127f17313a1e1de3cbac027765bfe847b786c160
Alois Schlögl <alois.schloegl@ist.ac.at>
2013-03-01 16:14:05 +0100

    hdr2ascii: fix display of EVENT.DUR value of sparse samples

--- biosig4matlab/t200_FileAccess/hdr2ascii.m
+++ biosig4matlab/t200_FileAccess/hdr2ascii.m
@@ -259,10 +259,11 @@ for k = 1:length(HDR.EVENT.POS);
 		t = datevec(HDR.EVENT.POS(k)/(24*3600*HDR.SampleRate)+T0);
 	end
 	fprintf(fid,'0x%04x\t%9i\t%04i-%02i-%02i %02i:%02i:%07.4f',[HDR.EVENT.TYP(k),HDR.EVENT.POS(k),t(1:6)]); 
-%%	fprintf(fid,'0x%04x\t%9i',[HDR.EVENT.TYP(k),HDR.EVENT.POS(k)]'); 
 	if isfield(HDR.EVENT,'CHN')
 		if ~isempty(HDR.EVENT.CHN)
-			fprintf(fid,'\t%i\t%i',HDR.EVENT.CHN(k),HDR.EVENT.DUR(k)); 
+			DUR = HDR.EVENT.DUR(k);
+			if (HDR.EVENT.TYP(k)==hex2dec('7fff')) DUR=0; end;
+			fprintf(fid,'\t%i\t%i', HDR.EVENT.CHN(k), DUR);
 		end;
 	else 
 		fprintf(fid,'\t-\t-');	
