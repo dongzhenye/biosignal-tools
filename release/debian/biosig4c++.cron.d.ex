e99644770e9975a813da322a51723d74775dac09
Alois Schlögl <alois.schloegl@ist.ac.at>
2014-06-23 08:30:47 +0200

    debian/lintian: remove example files

--- release/debian/biosig4c++.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the biosig4c++ package
-#
-0 4	* * *	root	[ -x /usr/bin/biosig4c++_maintenance ] && /usr/bin/biosig4c++_maintenance
--- release/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(biosig4c++):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="biosig4c++" command="/usr/bin/biosig4c++"
