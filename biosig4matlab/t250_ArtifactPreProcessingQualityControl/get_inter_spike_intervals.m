6bb312b59283c38df424ce44f48a14f1e249b20d
Alois Schlögl <alois.schloegl@ist.ac.at>
2014-07-11 14:51:13 +0200

    [B4M] new function GET_INTER_SPIKE_INTERVALS for extracting interspike intervals

---
+++ biosig4matlab/t250_ArtifactPreProcessingQualityControl/get_inter_spike_intervals.m
@@ -0,0 +1,61 @@
+function [ISI] = get_inter_spike_intervals(F)
+% GET_INTER_SPIKE_INTERVALS compute series of interspike 
+%  intervals from the eventtable of  a biosignal data set 
+%  loaded with SOPEN or SLOAD. 
+%
+% Usage:
+%    [ISI] = get_inter_spike_intervals(F)
+% 
+% Input:  
+% 	F filename
+% Output:  
+%	ISI series (vector) of interspike intervals in seconds  
+%
+% See also: SOPEN, SLOAD, DETECT_SPIKES_BURSTS, SPIKE2BURSTS
+
+%    Copyright (C) 2014 by Alois Schloegl <alois.schloegl@ist.ac.at>
+%    This is part of the BIOSIG-toolbox http://biosig.sf.net/
+%
+%    BioSig is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    BioSig is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with BioSig.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+if ischar(F) && exist(F,'file');
+	HDR = sopen(F); 
+	HDR = sclose(HDR); 
+
+elseif isstruct(HDR) && isfield(HDR,'EVENT') && isfield(HDR.EVENT,'POS') && isfield(HDR.EVENT,'TYP')
+	; %% everything fine - continue
+else 
+	error('invalid input arguments'); 
+end; 
+
+%% select only spikes and segment breaks. 
+ix = ( ( HDR.EVENT.TYP == hex2dec('0201') ) | ( HDR.EVENT.TYP == hex2dec('7ffe') ) );
+EVENT.TYP = HDR.EVENT.TYP(ix);
+EVENT.POS = HDR.EVENT.POS(ix);
+
+%% allocate memory and initialize with NaN
+T = repmat(NaN,length(EVENT.POS),1);
+
+%% select spike posititions, segment breaks remain NaN
+ix = find(EVENT.TYP==hex2dec('0201'));
+T(ix) = EVENT.POS(ix); 
+
+%% compute interspike intervals, segment breaks result in NaN. 
+d = diff(T); 
+
+%% remove meaningless intervals caused by segment breaks, and normalize to seconds. 
+ISI = d(~isnan(d))/HDR.SampleRate;
+
