be054226246a4cf1147b0f91d94ac9aae129531d
Alois Schloegl <alois.schloegl@ist.ac.at>
2014-07-18 00:47:40 +0200

    ABF2: more specific criteria; version is decoded to a more human-readable form

--- biosig4c++/biosig.c
+++ biosig4c++/biosig.c
@@ -1727,12 +1727,12 @@ HDRTYPE* getfiletype(HDRTYPE* hdr)
 #ifndef  ONLYGDF
 	else if (!memcmp(hdr->AS.Header, "ABF ", 4)) {
     	// else if (!memcmp(Header1,"ABF \x66\x66\xE6\x3F",4)) { // ABF v1.8
-	    	hdr->TYPE = ABF;
+		hdr->TYPE    = ABF;
     		hdr->VERSION = lef32p(hdr->AS.Header+4);
     	}
-	else if (!memcmp(hdr->AS.Header, "ABF2", 4)) {
-		hdr->TYPE = ABF2;
-    		hdr->VERSION = beu32p(hdr->AS.Header+4);
+	else if (!memcmp(hdr->AS.Header, "ABF2\x00\x00", 6) && ( hdr->AS.Header[7] < 10 ) ) {
+		hdr->TYPE    = ABF2;
+		hdr->VERSION = hdr->AS.Header[6] + ( hdr->AS.Header[7] / 10.0 );
     	}
     	else if (!memcmp(Header1+20,"ACR-NEMA",8))
 	    	hdr->TYPE = ACR_NEMA;
